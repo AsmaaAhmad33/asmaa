d6a18ec28f4306e5ed5535364a7e24040f29bf3d
AsmaaAhmad33 <smsmshab30@gmail.com>
2024-02-17 03:01:00 +0200

    000000

--- UPdataschool.php
+++ UPdataschool.php
@@ -4,7 +4,7 @@ include('./SCHOOL.PHP');
 
 $query = "UPDATE student
 SET Name= 'AHMAD shhab', email='AAA@gmail.com', password= '01202424'
-WHERE Concat info ='33' ";
+WHERE Concat info ='19' ";
 $stmt =$conn->prepare($query);
 
 $stmt->execute();
